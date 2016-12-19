865acbd1cdcde177f722055d03b4a4bad6e1ff12
7shi <7shi@live.jp>
2016-12-20 07:54:57 +0900

    combinationを追加

--- Math7.fsx
+++ Math7.fsx
@@ -26,3 +26,16 @@ module Math7 =
         prologue title
         for t in ts do t()
         epilogue()
+
+    let combination n i =
+        let rec f j = function
+        | [] -> []
+        | s::ss ->
+            match f (j + 1) ss with
+            | [] -> if s + 1 > j + n - i then [] else [s + 1 .. s + 1 + i - j]
+            | ss -> s::ss
+        let rec g list =
+            match f 1 list with
+            | [] -> [list]
+            | next -> list :: g next
+        g [1..i]
