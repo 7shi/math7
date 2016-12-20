f7a34057fe62995750fe4efbcfa23119c48b37e6
7shi <7shi@live.jp>
2016-12-21 01:40:34 +0900

    ベクトル積として表示

--- WedgeOct.fsx
+++ WedgeOct.fsx
@@ -3,10 +3,10 @@
 #load "Math7.Exterior.fsx"
 open Math7
 
-let showProd pfx op f g sort1 sort2 filter nl a b =
+let showProd op f g sort1 sort2 filter nl a b =
     let sa = Term.strs f a |> Term.bracket
     let sb = Term.strs f b |> Term.bracket
-    printfn @"&%s%s%s%s \\" pfx sa op sb
+    printfn @"&%s%s%s \\" sa op sb
     Term.splits a
     |> List.mapi (fun i a ->
         Term.splits b
@@ -22,8 +22,8 @@ for n = 1 to 7 do
     Math7.prologue (sprintf "## %d次元" n)
     let a = [for i in [1..n] -> term(1, [sprintf "a_%d" i], [i])]
     let b = [for i in [1..n] -> term(1, [sprintf "b_%d" i], [i])]
-    showProd "" "∧" (Term.vec "∧") (Term.fromE >> Exterior.simplify)
+    showProd "∧" (Term.vec "∧") (Term.fromE >> Exterior.simplify)
         id Term.byIndexSign (fun _ -> true) (fun x -> x % 3 = 0) a b
-    showProd "Im" "" (Term.vec "") Octonion.prod id Term.byIndexSign
+    showProd "×" (Term.vec "×") Octonion.prod id Term.byIndexSign
         (fun (e, _) -> e.E <> []) (fun _ -> true) a b
     Math7.epilogue()
