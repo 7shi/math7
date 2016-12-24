8d2ed27202cdf4a6c567c13d3152b95ea1add3ed
7shi <7shi@live.jp>
2016-12-24 22:10:56 +0900

    コードの整理

--- ExtProduct.fsx
+++ ExtProduct.fsx
@@ -2,68 +2,61 @@
 open Math7
 
 Term.showProd "## 代数式" ""
-    (fun es -> es |> Seq.map (fun e -> [""; "x"; "y"; "z"].[e]) |> Term.strPower)
-    (fun es ->
-        match es with
-        | [2; 1] | [3; 2] | [1; 3] -> term(1, [], List.rev es)
-        | _ -> term(1, [], es))
-    (fun xs ->
-        match xs with
-        | [x; y] when x = y -> (-2, xs)
-        | _                 -> (-1, xs))
+    (Seq.map (fun e -> [""; "x"; "y"; "z"].[e]) >> Term.strPower)
+    (function
+    | [x; y] when y % 3 + 1 = x -> Term.fromE [y; x]
+    | es -> Term.fromE es)
+    (function
+    | [x; y] as xs when x = y -> (-2, xs)
+    |           xs            -> (-1, xs))
     Term.byIndexSign ((=) 3)
     [for i in [1..3] -> term(1, [sprintf "a_%d" i], [i])]
     [for i in [1..3] -> term(1, [sprintf "b_%d" i], [i])]
 
 Term.showProd "## テンソル積" @"\otimes "
     (Term.vec @"\otimes ")
-    (fun es -> term(1, [], es))
-    id Term.byIndexSign (fun _ -> false)
+    Term.fromE id Term.byIndexSign (fun _ -> false)
     [for i in [1..3] -> term(1, [sprintf "a_%d" i], [i])]
     [for i in [1..3] -> term(1, [sprintf "b_%d" i], [i])]
 
 Term.showProd "## ウェッジ積" "∧"
     (Term.vec "∧")
-    (fun es ->
-        match es with
-        | [x; y] when x = y -> term.Zero
-        | [2; 1] | [3; 2] | [1; 3] -> term(-1, [], List.rev es)
-        | _ -> term(1, [], es))
+    (function
+    | [x; y] when x = y -> term.Zero
+    | [x; y] when y % 3 + 1 = x -> term(-1, [], [y; x])
+    | es -> Term.fromE es)
     id Term.byIndexSign (fun _ -> false)
     [for i in [1..3] -> term(1, [sprintf "a_%d" i], [i])]
     [for i in [1..3] -> term(1, [sprintf "b_%d" i], [i])]
 
 Term.showProd "## ベクトル積" "×"
     (Term.vec "×")
-    (fun es ->
-        match es with
-        | [x; y] when x = y -> term.Zero
-        | [1; 2] -> term( 1, [], [3]) | [2; 3] -> term( 1, [], [1]) | [3; 1] -> term( 1, [], [2])
-        | [2; 1] -> term(-1, [], [3]) | [3; 2] -> term(-1, [], [1]) | [1; 3] -> term(-1, [], [2])
-        | _ -> term(1, [], es))
+    (function
+    | [x; y] when x = y -> term.Zero
+    | [x; y] when x % 3 + 1 = y -> term( 1, [], [y % 3 + 1])
+    | [x; y] when y % 3 + 1 = x -> term(-1, [], [x % 3 + 1])
+    | es -> Term.fromE es)
     id Term.byIndexSign (fun _ -> false)
     [for i in [1..3] -> term(1, [sprintf "a_%d" i], [i])]
     [for i in [1..3] -> term(1, [sprintf "b_%d" i], [i])]
 
 Term.showProd "## 幾何学積" ""
     (Term.vec "")
-    (fun es ->
-        match es with
-        | [x; y] when x = y -> term.One
-        | [2; 1] | [3; 2] | [1; 3] -> term(-1, [], List.rev es)
-        | _ -> term(1, [], es))
+    (function
+    | [x; y] when x = y -> term.One
+    | [x; y] when y % 3 + 1 = x -> term(-1, [], [y; x])
+    | es -> Term.fromE es)
     id Term.byIndexSign ((=) 1)
     [for i in [1..3] -> term(1, [sprintf "a_%d" i], [i])]
     [for i in [1..3] -> term(1, [sprintf "b_%d" i], [i])]
 
 Term.showProd "## 四元数" ""
-    (fun es -> es |> Seq.map (fun e -> [""; "i"; "j"; "k"].[e]) |> Term.strPower)
-    (fun es ->
-        match es with
-        | [x; y] when x = y -> term -1
-        | [1; 2] -> term( 1, [], [3]) | [2; 3] -> term( 1, [], [1]) | [3; 1] -> term( 1, [], [2])
-        | [2; 1] -> term(-1, [], [3]) | [3; 2] -> term(-1, [], [1]) | [1; 3] -> term(-1, [], [2])
-        | _ -> term(1, [], es))
+    (Seq.map (fun e -> [""; "i"; "j"; "k"].[e]) >> Term.strPower)
+    (function
+    | [x; y] when x = y -> term -1
+    | [x; y] when x % 3 + 1 = y -> term( 1, [], [y % 3 + 1])
+    | [y; x] when x % 3 + 1 = y -> term(-1, [], [y % 3 + 1])
+    | es -> Term.fromE es)
     id Term.byIndexSign ((=) 1)
     [for i in [1..3] -> term(1, [sprintf "a_%d" i], [i])]
     [for i in [1..3] -> term(1, [sprintf "b_%d" i], [i])]
